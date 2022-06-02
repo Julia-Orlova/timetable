c2fa3df6206b414946f12ceaad8a9979a51f2f20
Julia-Orlova <66700142+Julia-Orlova@users.noreply.github.com>
2022-06-02 15:13:21 +0300

    Update config.js

--- docs/software/config.js
+++ docs/software/config.js
@@ -1,5 +1,8 @@
 module.exports = {
     server:{
+        /* if there is a system variable process.env.PORT, then the server is started on this port,
+        otherwise (development mode on the local computer) - on port 8080
+        */
         port: process.env.PORT || 8080,
         staticPath: "./assets"
     }
